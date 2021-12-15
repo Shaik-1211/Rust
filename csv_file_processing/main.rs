58cc6687a66a647b2bca777ed9aca49ea2194e1e
Shaik-1211 <88929013+Shaik-1211@users.noreply.github.com>
2021-12-15 19:28:51 +0530

    Managed main.rs using another file lib.rs

--- csv_file_processing/main.rs
+++ csv_file_processing/main.rs
@@ -2,30 +2,31 @@ extern crate csv;//External crate {csv="1.1"} has been added in cargo.toml file
 extern crate serde;//External crate {serde="1"} has been added in cargo.toml file
 #[macro_use]
 extern crate serde_derive;//External crate {serde_derive="1"} has been added in cargo.toml file
-use std::error::Error;
-// use std::process;
 use std::io;
+use std::error::Error;
+use CSV;
+mod lib;
 
 #[derive(Debug,Deserialize)]
 #[serde(rename_all="PascalCase")]
-struct Record 
+pub struct Record 
 {   
     #[serde(rename = "Student_Name")]    
-    studentname:String,
+    pub studentname:String,
     #[serde(rename = "Student_Id")]
-    studentid:i64,
+    pub studentid:i64,
     #[serde(rename = "English_Score")]
-    english_score:i64,
+    pub english_score:Option<i64>,
     #[serde(rename = "Maths_Score")]
-    maths_score:i64,
+    pub maths_score:Option<i64>,
    #[serde(rename = "Science_Score")]
-    science_score:i64,
+    pub science_score:Option<i64>,
    #[serde(rename = "Social_Score")]
-    social_score:i64,
+    pub social_score:Option<i64>,
     #[serde(rename="Computer_Score")]
-    computer_score:i64,
+    pub computer_score:Option<i64>,
 }
-fn read_csv(path:&str,name:String)->Result<(),Box<dyn Error>>
+pub fn read_csv(path:&str,name:String)->Result<(),Box<dyn Error>>
 {
     let mut reader=csv::Reader::from_path(path)?;
     let _headers=reader.headers()?;
@@ -50,30 +51,7 @@ fn read_csv(path:&str,name:String)->Result<(),Box<dyn Error>>
     Ok(())
 }
 
-fn count(path:&str)->Result<(),Box<dyn Error>>
-{
-    let mut reader=csv::Reader::from_path(path)?;
-    let mut store:Vec<String>=Vec::new();
-    let mut size=0;
-    for result in reader.records()
-    { let record=result?;
-        let  mut student_data=String::new();
-        student_data=record[0].to_string();
-        store.push(student_data);
-        size=record.len();
-    
-    }
-    let mut reader=csv::Reader::from_path(path)?;
-    let headers=reader.headers()?;
-    // println!("record size is {:?}",size);
-    for i in 0..=size-1
-    {   
-             println!("{:?} : {:?}",&headers[i],store.len());
-    }
-
-    Ok(())
-}
-fn search_through_id(path:&str,id:i64)->Result<(),Box<dyn Error>>
+pub fn search_through_id(path:&str,id:i64)->Result<(),Box<dyn Error>>
 {
     let mut reader=csv::Reader::from_path(path)?;
     let mut count=0;
@@ -88,118 +66,40 @@ fn search_through_id(path:&str,id:i64)->Result<(),Box<dyn Error>>
         }
         
     }
-    if count>1
+    if count>15
     {println!("No record found for the student_id entered.");}
     Ok(())
 }
 
-fn headers(path:&str)->Result<(),Box<dyn Error>>
+pub fn null_count(path:&str)->Result<(),Box<dyn Error>>
 {
     let mut reader=csv::Reader::from_path(path)?;
-    let headers=reader.headers()?;
-    println!("{:?}",&headers[0]);
-   println!("{:#?}",headers);//Only prints the header of the csv file
-   Ok(())
-}
+    let mut count=0;
+    let mut count_english=0;
+    let mut count_maths=0;
+    let mut count_science=0;
+    let mut count_social=0;
+    let mut count_computer=0;
 
-// The get_column function will print the column (choosen by the user) of the csv file.
-fn get_column(path:&str,query:String)->Result<(),Box<dyn Error>>
-{
-    let mut rdr=csv::Reader::from_path(path)?;
-
-    for result in rdr.records()
-    {
-        let record = result?;
-        let student_name=&record[0];
-        if query=="student_name"{
-        println!("student_name:{:?}",student_name);}
-        let student_id=&record[1];
-        if query=="student_id"{
-        println!("student_Id:{:?}",student_id);}
-        let english_score=&record[2];
-        if query=="english_score"{
-            println!("english_score:{:?}",english_score);
-        }
-        let maths_score=&record[3];
-        if query=="maths_score"{
-            println!("maths_score:{:?}",maths_score);
-        }
-        let science_score=&record[4];
-        if query=="science_score"{
-            println!("science_score:{:?}",science_score);
-        }
-        let social_score=&record[5];
-        if query=="social_score"{
-            println!("social_score:{:?}",social_score);
-        }
-        let computers_score=&record[6];
-        if query=="computers_score"{
-            println!("computers_score:{:?}",computers_score);
-        }
+    for result in reader.deserialize()
+    {let record:Record=result?;
+        if record.english_score==None
+        {count_english+=1;}
+        if record.maths_score==None
+        {count_maths+=1;}
+        if record.science_score==None
+        {count_science+=1;}
+        if record.social_score==None
+        {count_social+=1;}
+        if record.computer_score==None
+        {count_computer+=1;}
     }
-    Ok(())
-}
+    println!("Total null values in english_score = {:?}",count_english);
+    println!("Total null values in maths_score = {:?}",count_maths);
+    println!("Total null values in science_score = {:?}",count_science);
+    println!("Total null values in social_score = {:?}",count_social);
+    println!("Total null values in computer_score = {:?}",count_computer);
 
-// This check function will check if there is any duplicated record and print the student name of the duplicated record.
-fn check(path:&str)->Result<(),Box<dyn Error>>
-{
-    let mut rdr = csv::Reader::from_path(path)?;
-    let mut store:Vec<String>=Vec::new();
-    for result in rdr.records()
-    {   
-        let record=result?;
-        let  mut student_name=String::new();
-        student_name=record[0].to_string();
-        store.push(student_name);
-    }
-    let mut var1=false;
-    let mut i=1;
-    let mut var2=Vec::new();
-
-    for name in &store
-        {
-            for j in &store[i..]
-            { if name==j {var1=true;
-                          var2.push(j);}
-                else {var1=false;}
-            }
-            i+=1;
-        }
-        if var1==true
-        {println!("Duplicate data found and the student_name of the records repeated : {:?}",var2); }
-        else
-        {println!("No Duplicate data found");}
-    Ok(())
-}
-
-
-
-fn choice2()->Result<String,Box<dyn Error>>
-{
-    println!("The columns in the file 'studentinfo.csv' are : ");
-    if let Err(e)=headers("studentinfo.csv"){
-        println!("Error occured {:?}",e);
-    }
-    println!("Enter the column name you need to be displayed:");
-    let mut b=String::new();
-    io::stdin().read_line(&mut b)?;
-    let column=b.trim().to_lowercase();
-    Ok(column)
-
-}
- 
-fn get_id()->Result<i64,Box<dyn Error>>
-{   println!("Enter Student Id : ");
-    let mut c=String::new();
-    io::stdin().read_line(&mut c)?;
-    let num:i64=c.trim().parse().expect("Enter an integer");
-
-    Ok(num)
-}
-fn write(path:&str)->Result<(),Box<dyn Error>>
-{
-    let mut wtr=csv::Writer::from_path(path)?;
-    wtr.write_record(&["umar","117","56","74","87","94"])?;
     Ok(())
 }
 fn main()
@@ -208,8 +108,13 @@ fn main()
     loop
   {
     println!("Choose the action to perform");
-    println!("1.Get a record of a student(use student name)\t2.Get a record of a student(use student id)\t3.Display a column\t
-4.Check whether a record has repeated\t5.print the total number of fields in each column" );
+    println!("
+    1.Get a record of a student(use student name)
+    2.Get a record of a student(use student id)     
+    3.Display a column  
+    4.Check whether a record has repeated
+    5.print the total number of fields in each column 
+    6.Print the null values in each column");
     let mut option=String::new();
     io::stdin().read_line(&mut option).expect("invalid input");
     let choice:i32=option.trim().parse().expect("Enter a valid option number");
@@ -224,8 +129,9 @@ fn main()
         break;}
 
     else if choice==2
-    {   let id=get_id();
+    {   let id=CSV::get_id();
         match id
+
         {
             Ok(num)=>if let Err(e)=search_through_id("studentinfo.csv", num){
                 println!("{:?}",e);},
@@ -234,10 +140,10 @@ fn main()
         
     break;}
     else if choice==3{
-    let column=choice2();
+    let column=CSV::choice2();
     match column
     {
-        Ok(column_name)=>if let Err(e)=get_column("studentinfo.csv",column_name){
+            Ok(column_name)=>if let Err(e)=CSV::get_column("studentinfo.csv",column_name){
                 println!("{:?}",e);},
         Err(e)=>{println!("Error occured in choice2 {:?}",e);},
     }
@@ -245,7 +151,7 @@ fn main()
 
 
     else if choice==4{
-    if let Err(e)=check("studentinfo.csv"){
+    if let Err(e)=CSV::check("studentinfo.csv"){
         println!("{:?}",e);
     }break;}
     
@@ -257,17 +163,23 @@ fn main()
 
     else if choice==5
     {
-        count("studentinfo.csv");
+        if let Err(e)=CSV::count("studentinfo.csv"){
+            println!("Error while counting duplicate data {:?}",e);
+        }
+        break;
+    }
+
+    else if choice==6
+    {
+        if let Err(e)=null_count("studentinfo.csv"){
+            println!("Error while count null vaues : {:?}",e);
+        }
         break;
     }
     else {println!("Choose from the above 5 options");
     
-
     if chances==1{break;}
     chances-=1;}
   }
-  
-
-
 }
 
