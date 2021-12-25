6c70ff089bf923fc02a4711fb13560b53715a76c
Shaik-1211 <88929013+Shaik-1211@users.noreply.github.com>
2021-12-25 19:32:33 +0530

    A program to process csv file and write the output to another file

--- csv_file2/main.rs
+++ csv_file2/main.rs
@@ -1,23 +1,23 @@
 extern crate csv;
 use std::error::Error;
 
-// #[warn(unused_assignments)]
-// #[warn(unused_allocation)]
 fn write_csv(inputpath: &str,outputpath:&str)->Result<(),Box<dyn Error>>{
     let mut rdr=csv::Reader::from_path(inputpath)?;
     let mut wtr=csv::Writer::from_path(outputpath)?;
-    wtr.write_record(&["first_name","Last_name","Fullname"])?;
-    // let mut full_name=String::new();
-    // let mut first_name=String::new();
-    // let mut last_name=String::new();
+    wtr.write_record(&["first_name","Last_name","Fullname","Dot extension","Length of dot extension"])?;//writes header in ouput.csv
     for result in rdr.records() {
         let record=result?;
-        // first_name=record[0].to_string();
-        // last_name=record[1].to_string();
-        // full_name=record[0].to_string()+" "+&record[1].to_string();
-    // wtr.write_record(&csv::StringRecord::from(vec![first_name, last_name,full_name]))?;
+        let mut first_name=record[0].to_string();
+        first_name.push('.');
+        let mut var=String::new();
+        let (first,last)=first_name.split_at(1);//splitting the first name at index 1 
+        var.push_str(first);
+        first_name.push_str(var.as_str());//Adding the first letter of the full name
+        println!("{:?}",first_name);
+        let length=first_name.len().to_string();//Calculating the length of Dot extension
+        //writing the data to the output csv file
     wtr.write_record(&csv::StringRecord::from(
-        vec![record[0].to_string(),record[1].to_string(),record[0].to_string()+" "+&record[1].to_string()]))?;
+        vec![record[0].to_string(),record[1].to_string(),record[0].to_string()+" "+&record[1].to_string(),first_name,length]))?;
     }
     Ok(())
 }
